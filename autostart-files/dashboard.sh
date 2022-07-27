90e858f2995d3b6e9d26fc686f045ac0e9fbb6dd
XRayrahman <rayadityarahman14@gmail.comgit>
2022-07-27 17:22:19 +0700

    update tutorial and add structure

--- autostart-files/dashboard.sh
+++ autostart-files/dashboard.sh
@@ -5,7 +5,7 @@ xdotool mousemove_relative --polar 180 400 # second method for moving mouse poin
 # sudo xrandr --newmode "800x480_60.00"   29.50  800 824 896 992  480 483 493 500 -hsync +vsync # set new resolution (800x480 60fps)
 # sudo xrandr --addmode $MONITOR "800x480_60.00" # set $MONITOR to default display ex. HDMI-1
 # sudo xrandr --output $MONITOR --mode "800x480_60.00" # set the new created resolution
-# source /home/pi/dashboard/bin/activate # change depends on directory
+# source /home/pi/dashboard/bin/activate # change de pends on directory
 cd /home/orangepi/dashboardpy_motorcycle_bright/ # change depends on directory
 # cat /dev/ttyUSB0 &
 # cat /dev/ttyUSB1 &
