10366eb2c424eb7b57bf716c6ad1cde284efd43b
XRayrahman <rayadityarahman14@gmail.comgit>
2022-07-27 17:40:56 +0700

    update resolution orientation

--- autostart-files/dashboard.sh
+++ autostart-files/dashboard.sh
@@ -2,9 +2,10 @@
 
 # xte 'mousemove 900 50' # first method for moving mouse pointer
 xdotool mousemove_relative --polar 180 400 # second method for moving mouse pointer
-# sudo xrandr --newmode "800x480_60.00"   29.50  800 824 896 992  480 483 493 500 -hsync +vsync # set new resolution (800x480 60fps)
-# sudo xrandr --addmode $MONITOR "800x480_60.00" # set $MONITOR to default display ex. HDMI-1
-# sudo xrandr --output $MONITOR --mode "800x480_60.00" # set the new created resolution
+# xrandr --newmode "800x480_60.00"   29.50  800 824 896 992  480 483 493 500 -hsync +vsync # set new resolution (800x480 60fps)
+# xrandr --addmode HDMI-1 "800x480_60.00" # set $MONITOR to default display ex. HDMI-1
+# xrandr --output HDMI-1 --mode "800x480_60.00" # set the new created resolution
+# xrandr -o inverted
 # source /home/pi/dashboard/bin/activate # change de pends on directory
 cd /home/orangepi/dashboardpy_motorcycle_bright/ # change depends on directory
 # cat /dev/ttyUSB0 &
